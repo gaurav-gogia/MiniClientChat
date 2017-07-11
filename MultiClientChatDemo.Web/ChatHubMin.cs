540c83ab7855a101b934053d74b7a748836df9b5
Gaurav Gogia <desmondmiles36@gmail.com>
2017-07-11 10:15:48 +0530

    security & typist update

--- MultiClientChatDemo.Web/ChatHubMin.cs
+++ MultiClientChatDemo.Web/ChatHubMin.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+
+
 namespace MultiClientChatDemo.Web
 {
     [HubName("chat")]
@@ -8,19 +10,19 @@ namespace MultiClientChatDemo.Web
         static int _counter = 0;
         public void SendMessage(string sender, string msg, string reciever)
         {
-            Clients.Others.stopThatTypingThing();
+            Clients.Others.stopThatTypingThing();            
             Clients.Others.receiveMessage(sender, msg, reciever);
-            Clients.Caller.meraMessage(sender, msg);            
+            Clients.Caller.meraMessage(sender, msg, reciever);            
         }
 
-        public void IsThisPersonTyping(string sender)
+        public void YesTyping(string sender)
         {
             Clients.Others.getTypist(sender);
         }
 
         public void NotTyping()
         {
-            Clients.Others.stopThatTypingThing();
+            Clients.All.typingStopped();
         }
 
         public void record(string peronName)
@@ -28,14 +30,18 @@ namespace MultiClientChatDemo.Web
             _counter++;
             Clients.All.receiveHit(_counter);
             Clients.All.updateUserList(peronName, "entered the room");
+
+            int[] iv = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            int padding = 5;
+
+            Clients.All.getIVPadding(iv, padding);
         }
 
         public void disconnectedFromServer(string sender)
         {
-            _counter--;
-            Clients.All.stopThatTypingThing();
+            _counter--;            
             Clients.All.okieByeBye(sender, "left the room");
             Clients.All.receiveHit(_counter);
-        }
+        }        
     }
 }
